d85ced8331404d3022f48b179aa8cb9e198de08a
b.sik <tjswl756@naver.com>
2021-07-26 20:02:37 +0900

    Create BOJ2343.java

---
+++ BOJ2343.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int n, m, k;
+    static ArrayList<Integer> arr=new ArrayList<>();
+    static int left, right, mid;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int sum=0;
+        int result=0;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr.add(Integer.parseInt(st.nextToken()));
+            sum+=arr.get(i);
+        }
+
+        left = 1;
+        right = sum;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if(check(mid)){
+                result=mid;
+                right=mid-1;
+            }else
+                left=mid+1;
+        }
+
+        System.out.println(result);
+    }
+    static boolean check(int mid){
+        int sum=mid;
+        int cnt=1;
+        for(int i=0; i<arr.size(); i++){
+            if(mid<arr.get(i))
+                return false;
+
+            if(sum- arr.get(i)<0){
+                sum=mid;
+                cnt++;
+            }
+            sum-=arr.get(i);
+        }
+        return m>=cnt;
+    }
+}
