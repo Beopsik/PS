edb9c163144f02aa50cec895ef6ed8372797ea64
b.sik <tjswl756@naver.com>
2022-08-22 11:50:47 +0900

    Create BOJ11279.py

---
+++ BOJ11279.py
@@ -0,0 +1,19 @@
+from queue import PriorityQueue
+from sys import stdin        
+
+n=int(stdin.readline())
+operation=PriorityQueue()
+result=[]
+
+for i in range(n):
+    value=int(stdin.readline())
+    if value==0:
+        if operation.empty():
+            result.append("0")
+        else:
+            result.append(str(operation.get())[1:])
+    else:
+        operation.put(-value)
+
+result="\n".join(result)
+print(result)
