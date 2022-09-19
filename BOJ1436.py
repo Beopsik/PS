7487562908743558ed81176aaa30fccd91f52fe8
b.sik <tjswl756@naver.com>
2022-09-19 10:14:14 +0900

    Create BOJ1436.py

---
+++ BOJ1436.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+n=int(stdin.readline())
+count=0
+num=666
+while True:
+    if '666' in str(num):
+        count+=1
+    if count==n:
+        print(num)
+        break
+    num+=1
